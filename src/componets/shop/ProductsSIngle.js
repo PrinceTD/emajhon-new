4099c2a2376d6d65c08bf754bd0a3e150d2ace82
PrinceTD <prince.talukder001@gmail.com>
2022-06-16 11:37:23 +0600

    added all

--- src/componets/shop/ProductsSIngle.js
+++ src/componets/shop/ProductsSIngle.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './product.css'
 
 export default function ProductsSIngle(props) {
+    // console.log(props)
     const { name, img, price, stock, seller } = props.product
 
     return (
         <div className='product'>
-            <img src={img}></img>
+            <img src={img} width='218px' height='218px' />
            <div className='allProduct'>
            <h4 className='productName'>{name}</h4>
            <p><small>by: {seller}</small></p>
@@ -15,7 +16,8 @@ export default function ProductsSIngle(props) {
 
            </div>
            <p>only {stock} left in stock - order soon</p>
-           <button className='btn'>add to card</button>
+           <button
+           onClick={()=> props.handerAddToCard(props.product)} className='btn'>add to card</button>
            </div>
             
         </div>
--- src/componets/shop/Shop.js
+++ src/componets/shop/Shop.js
@@ -1,29 +1,37 @@
 import React, { useEffect, useState } from 'react'
+import Cart from './cart/Cart';
 import ProductsSIngle from './ProductsSIngle';
 
 import './Shop.css'
 
 export default function Shop() {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('./products.JSON')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+
+    const handerAddToCard = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shopContainer'>
             <div className='product-container'>
                 <h3> Product: {products.length}</h3>
                 {
-                    products.map(product => <ProductsSIngle product={product}
+                    products.map(product => <ProductsSIngle
+                        product={product}
                         key={product.key}
+                        handerAddToCard={handerAddToCard}
                     ></ProductsSIngle>)
                 }
             </div>
             <div className='cart-container'>
-                <h3> Order Summary</h3>
-                <h5>Item order</h5>
+               <Cart cart={cart}></Cart>
             </div>
         </div>
     )
